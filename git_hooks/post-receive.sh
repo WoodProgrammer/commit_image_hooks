c4d80cebaef5b532126d4d4e11cf4fc493431957
admanage <asddeneme3@gmail.com>
2017-10-06 19:20:53 +0300

    POst Receive Hooks added

---
+++ git_hooks/post-receive.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+repo_name=$(pwd |awk '{split($0,a,"/"); print a[4]}')
+repo_docs_name=$(echo $repo_name |awk '{split($0,a,".");print a[1]}')
+
+
+git --work-tree=/home/git/docs/$repo_docs_name --git-dir=/home/git/version/$repo_name checkout -f
+
+
+
