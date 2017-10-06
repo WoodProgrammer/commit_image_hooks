3ffa17e8d45864a0b629a73bbb98bf1db718679d
admanage <asddeneme3@gmail.com>
2017-10-06 22:46:49 +0300

    Junk Fİles deleted auto builder added but versioning is from a file .g

--- git_hooks/post-receive.sh
+++ git_hooks/post-receive.sh
@@ -3,9 +3,12 @@ repo_name=$(pwd |awk '{split($0,a,"/"); print a[4]}')
 repo_docs_name=$(echo $repo_name |awk '{split($0,a,".");print a[1]}')
 
 git --work-tree=/home/git/docs/$repo_docs_name --git-dir=/home/git/version/$repo_name checkout -f
+version_number=$(cat version)
+docker build -t $DOCKER_USER/$repo_docs_name:$version_number /home/git/docs/$repo_docs_name/.  
 
 
-docker build /home/git/docs/$repo_docs_name
+docker tag $USERNAME/$IMAGE:$version_number  $USERNAME/$IMAGE:$version_number
 
+docker push $USERNAME/$IMAGE:$version_number
 
 
