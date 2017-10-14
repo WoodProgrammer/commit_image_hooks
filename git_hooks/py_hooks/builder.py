2750e3fc6e14e8cc3ae7eba5b2e8c75ecba84f73
admanage <asddeneme3@gmail.com>
2017-10-15 02:33:43 +0300

    BugFixed

--- git_hooks/py_hooks/builder.py
+++ git_hooks/py_hooks/builder.py
@@ -1,21 +1,42 @@
 import docker
-import os,time
+import os, time
+import requests
+
 
 class Builder:
-    docker_path = lambda path: path.split("/")
     def __init__(self):
         self.client = docker.from_env()
 
-    def build_image(self):
-        file_path = self.docker_path(os.getcwd())
-        try:
-            image = self.client.build(path = file_path[5])
-            return "Image {} builded . hour: {}  date :  {} ".format(image.id,time.strftime("%H:%M:%S"),time.strftime("%d/%m/%Y"))
-        except:
-            return "Image building attempt failed on last commit  {}".format()
+    @staticmethod
+    def get_docker_file_place():
+            real_path = os.getcwd()
 
+
+
+
+    def build_image(self):
+        file_path = os.getcwd()
+        try:
+
+            image = self.client.images.build(path=self.get_docker_file_place())
+
+            try:
+                r = requests.put('http://localhost:5000/emirozbir/{}'.format(image.id), data={"status": "PASSED "})
+            except:
+                return "ERROR occured while PUT the data to the API "
+
+            return "Image {} builded . hour: {}  date :  {} ".format(image.id, time.strftime("%H:%M:%S"),
+                                                                     time.strftime("%d/%m/%Y"))
+
+        except:
+
+            r = requests.put('http://localhost:5000/emirozbir/failed_images/', data={"image":"COMMIT_NUMBER","""status": "FAILED "})
+            print r.status_code
+
+            return "Image building attempt failed on last commit  {}".format('hebele')
 
             ##requestss to the apis
 
 
-
+x = Builder()
+print(x.build_image())
