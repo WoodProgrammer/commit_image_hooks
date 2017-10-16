2e1830182b287d88c17167bf7556a361df747aa1
admanage <asddeneme3@gmail.com>
2017-10-17 00:18:29 +0300

    OS detection added to the scripts

--- registry_scripts/create_reg.sh
+++ registry_scripts/create_reg.sh
@@ -9,17 +9,30 @@ echo "Registry Setup is Starting"
 
 port_control=$(lsof -i tcp:5000)
 
+
 if [ "$port_control" = "" ]; then
     echo "Port is open for create registry ... "
 
     $(docker run -d -p 5000:5000 --name $registry_name registry:2 2>> registry_setup_error) 
 
     echo "For error log registry_setup_error."
-
  
 else
     echo "using another port registry "
 fi;
+
+if [ "$(uname -a|grep Linux)" == ""  ];then
+	echo "Your System is not Linux pull a registry please control security configs. HINT : "
+
+else
+	touch /etc/docker/daemon.js
+	echo >>' { "insecure-registries": ["192.168.0.13:5000"]   } '  > /etc/docker/daemon.js
+
+fi;	
+
+
+
+
 
 
 
