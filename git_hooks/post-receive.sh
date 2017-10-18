3ce7733c92f5641cb5b716aebbe42b2ba6888289
admanage <asddeneme3@gmail.com>
2017-10-18 03:39:20 +0300

    Bug Fixed

--- git_hooks/post-receive.sh
+++ git_hooks/post-receive.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-repo_name=$(pwd |awk '{split($0,a,"/"); print a[4]}')
-repo_docs_name=$(echo $repo_name |awk '{split($0,a,".");print a[1]}')
+repo_name=$(pwd |awk '{split($0,a,"/"); print a[5]}')
+mkdir -p /home/git/docs/$repo_name
 
+git_server_ip=$(hostname -I)
 
 if ! [ -t 0 ]; then
   read -a ref
@@ -10,38 +11,9 @@ fi
 IFS='/' read -ra REF <<< "${ref[2]}"
 branch="${REF[2]}"
 
-mkdir -p /home/git/test_branches/$repo_docs_name/$branch
-
-git --work-tree=/home/git/test_branches/$repo_docs_name --git-dir=/home/git/version/$repo_name checkout -f
+git --work-tree=/home/git/docs/$repo_name --git-dir=/home/git/version/$repo_name checkout -f
 
 
-if [ $branch == 'master' ]; then
-
-   docker build --tag "$repo_name-$branch"/home/git/test_branches/$repo_docs_name/$branch
-   build_status=$($?)
-   if [ build_status == 0 ]; then
-        echo "Master Branch image tagged  $repo_name-$branch LATEST . "
-
-        docker tag $repo_name-$branch:latest localhost:5000/$repo_name-$branch
-        docker push localhost:5000/$repo_name-$branch
-
-        echo "MASTER BRANCH DOCKER IMAGE PUSHED . "
-   fi
-else
-
-   docker build --tag "$repo_name-$branch"/home/git/test_branches/$repo_docs_name/$branch
-   build_status=$($?)
-   if [ build_status == 0 ]; then
-        echo "$branch  image tagged  $repo_name-$branch LATEST . "
-
-        docker tag $repo_name-$branch:latest localhost:5000/$repo_name-$branch
-        docker push localhost:5000/$repo_name-$branch
-
-        echo "$branch BRANCH DOCKER IMAGE PUSHED . "
-   fi
-
-
-fi
-
+curl http://192.168.0.13:7000/tobuild -d "branch_name=$branch" -d "repo_name=$repo_docs_name" -d "git_server=$git_server_ip" -X PUT
 
 
