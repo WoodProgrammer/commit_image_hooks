979de0329fa7140157db44d46164bd87d628cc9a
WoodProgrammer <emirozbir@hotmail.com>
2018-01-29 13:12:22 +0200

    README added Project deleted

--- git_server_script/create_version_and_docs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "REPO adı"
-read repo_adi
-
-
-ssh git@gitserver "mkdir -p /home/git/docs/$repo_adi \ mkdir -p /home/git/versions/$repo_adi.git "
-ssh git@gitserver "git init --bare /home/git/versions/$repo_adi.git/"
--- registry_scripts/test_if.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-port_control=$(lsof -i tcp:5000)
-
-if [ "$port_control" = "" ]; then 
-    echo "Port is open for create registry ... " 
-else
-    echo "using another port registry "  
-fi;
