f662e45a1b4bbb7f3c432c4c535bc1ef0106ee18
InitAck <emir@initack.net>
2018-04-05 02:02:01 +0300

    Hooks files added by [TEST STAGE]

--- hooks/pre-receive.sh
+++ hooks/pre-receive.sh
@@ -2,18 +2,23 @@
 echo "Adding CURRENT_PATH"
 
 CURRENT_PATH=$(pwd)
+REGISTRY_URL=$(echo $FERRUH_DOCKER_REGISTRY)
+VERSION_NUMBER=$(awk -F= '/VERSION_NUMBER/ { print $2 }' test/ferruh)
+TAG_NAME=$(awk -F= '/TAG_NAME/ { print $2 }' test/ferruh)
+
 echo $CURRENT_PATH | awk'{split($0,a,"/");}' 
 
 
-docker build ../../ 
+docker build -t  $TAG_NAME:$VERSION_NUMBER test/.
 OUT=$?
+
 
 if [ OUT -eq 0 ]; then
     echo "Dockerfile Building error"
 else
-    VERSION_NUMBER=$(awk -F= '/VERSION_NUMBER/ { print $2 }' ferruh)
-    TAG_NAME=$(awk -F= '/TAG_NAME/ { print $2 }' ferruh)
 
+    docker tag $TAG_NAME:$VERSION_NUMBER $REGISTRY_URL/$TAG_NAME:$VERSION_NUMBER
+    docker push $REGISTRY_URL/$TAG_NAME:$VERSION_NUMBER
 fi
 
 
